99f3438bc2de7e955740b2316da74e3438a51f81
MnCLosSantos <ccpautomotivegroup@gmail.com>
2025-06-26 03:55:22 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'MnC'
+description 'ped attack script'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+dependencies {
+    'ox_lib'
+}
+
+shared_script '@ox_lib/init.lua'
